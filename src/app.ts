3cf307332f56a24e17edb9d379ca2e3775c144ba
Erenshoyo <asiftauhid4665@gmail.com>
2026-05-21 15:17:00 +0600

    Working on authentication

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
-import express from "express";
+import express, { response, type Request, type Response } from "express";
 import { authRoutes } from "./modules/auth/auth.routes";
 import { issueRoutes } from "./modules/issues/issues.routes";
-
+import sendResponse from "./utils/response";
 
 const app = express();
 app.use(express.json());
 
 app.use(`/api/auth`, authRoutes);
 app.use(`/api/issues`, issueRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Server is running",
+  });
+});
 
 export default app;
