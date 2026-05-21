63849c7fb70b45341fa939a19455ccd33536be6e
Erenshoyo <asiftauhid4665@gmail.com>
2026-05-21 15:02:38 +0600

    Database added to the server

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,4 +5,33 @@ const pool = new Pool({
   connectionString: config.connection_string,
 });
 
-export default pool;
+export const initDB = async () => {
+  try {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    email VARCHAR(40) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    role VARCHAR(20) NOT NULL DEFAULT 'contributor' CHECK (role IN ('contributor','maintainer')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+`);
+
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS issues(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+    status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW())    
+`);
+    console.log(`Database connected successfully!`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import app from "./app";
 import config from "./config";
+import { initDB } from "./db/db";
 
 const PORT = config.port;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const main = () => {
+  initDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
 
-
+main();
